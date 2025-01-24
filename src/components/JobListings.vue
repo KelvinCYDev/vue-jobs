f470ce6de7e81ad92809936814769a53128d30c8
KelvinCYDev <kelvincy.dev@gmail.com>
2025-01-23 23:19:50 -0800

    Add: Proxy for subdirectory "/api"

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -22,7 +22,7 @@ const jobs = ref([]);
 
 onMounted(async () => {
   try {
-    const response = await axios.get('http://localhost:8000/jobs');
+    const response = await axios.get('/api/jobs');
     state.jobs = response.data;
   } catch (e) {
     console.error('Error fetching: ' + e)
